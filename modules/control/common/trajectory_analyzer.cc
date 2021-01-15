6f128829e4cda20d0b4e76720cd9e1683a0cf9c6
zhoulu980621 <74311102+zhoulu980621@users.noreply.github.com>
2021-01-15 18:29:26 +0800

    Update trajectory_analyzer.cc

--- modules/control/common/trajectory_analyzer.cc
+++ modules/control/common/trajectory_analyzer.cc
@@ -116,7 +116,7 @@ void TrajectoryAnalyzer::ToTrajectoryFrame(const double x, const double y,
 
   // the sin of diff angle between vector (cos_ref_theta, sin_ref_theta) and
   // (dx, dy)
-  double cross_rd_nd = dy * cos_ref_theta - dx * sin_ref_theta;
+  double cross_rd_nd = cos_ref_theta * dy - sin_ref_theta * dx;
   *ptr_d = cross_rd_nd;
 
   // the cos of diff angle between vector (cos_ref_theta, sin_ref_theta) and
